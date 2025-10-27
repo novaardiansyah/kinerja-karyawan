c1cb68c639e758724f8dd6938ab4f0db3d3826d6
novaardiansyah <novaardiansyah78@gmail.com>
2025-10-28 00:11:08 +0700

    refactor: implement component-based layout for performance management system
      - Break down monolithic dashboard into reusable components
      - Create sidebar with TOPSIS method navigation structure
      - Add authenticated user integration in navbar
      - Implement clean footer with version information
      - Optimize menu structure for employee performance assessment

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -14,25 +14,96 @@
     </div>
     <div class="sidebar-menu">
       <ul class="menu">
-        <li class="sidebar-title">Menu</li>
+        <li class="sidebar-title ps-0">Menu Utama</li>
 
         <li class="sidebar-item active">
-          <a href="index.html" class='sidebar-link'>
+          <a href="{{ url('/dashboard') }}" class='sidebar-link'>
             <i class="bi bi-grid-fill"></i>
             <span>Dashboard</span>
           </a>
         </li>
 
-        <li class="sidebar-item  has-sub">
-          <a href="#" class='sidebar-link'>
-            <i class="bi bi-stack"></i>
-            <span>Components</span>
+        <li class="sidebar-title ps-0">Manajemen Karyawan</li>
+
+        <li class="sidebar-item">
+          <a href="{{ url('/dashboard') }}" class='sidebar-link'>
+            <i class="bi bi-people-fill"></i>
+            <span>Data Karyawan</span>
           </a>
-          <ul class="submenu ">
-            <li class="submenu-item ">
-              <a href="component-alert.html">Alert</a>
-            </li>
-          </ul>
+        </li>
+
+        <li class="sidebar-item">
+          <a href="{{ url('/dashboard') }}" class='sidebar-link'>
+            <i class="bi bi-person-badge-fill"></i>
+            <span>Jabatan</span>
+          </a>
+        </li>
+
+        <li class="sidebar-item">
+          <a href="{{ url('/dashboard') }}" class='sidebar-link'>
+            <i class="bi bi-building"></i>
+            <span>Departemen</span>
+          </a>
+        </li>
+
+        <li class="sidebar-title ps-0">Penilaian Kinerja</li>
+
+        <li class="sidebar-item">
+          <a href="{{ url('/dashboard') }}" class='sidebar-link'>
+            <i class="bi bi-list-check"></i>
+            <span>Kriteria Penilaian</span>
+          </a>
+        </li>
+
+        <li class="sidebar-item">
+          <a href="{{ url('/dashboard') }}" class='sidebar-link'>
+            <i class="bi bi-star-fill"></i>
+            <span>Bobot Kriteria</span>
+          </a>
+        </li>
+
+        <li class="sidebar-item">
+          <a href="{{ url('/dashboard') }}" class='sidebar-link'>
+            <i class="bi bi-file-text-fill"></i>
+            <span>Input Nilai</span>
+          </a>
+        </li>
+
+        <li class="sidebar-item">
+          <a href="{{ url('/dashboard') }}" class='sidebar-link'>
+            <i class="bi bi-calculator-fill"></i>
+            <span>Perhitungan TOPSIS</span>
+          </a>
+        </li>
+
+        <li class="sidebar-item">
+          <a href="{{ url('/dashboard') }}" class='sidebar-link'>
+            <i class="bi bi-trophy-fill"></i>
+            <span>Hasil Perankingan</span>
+          </a>
+        </li>
+
+        <li class="sidebar-item">
+          <a href="{{ url('/dashboard') }}" class='sidebar-link'>
+            <i class="bi bi-file-earmark-bar-graph"></i>
+            <span>Laporan Kinerja</span>
+          </a>
+        </li>
+
+        <li class="sidebar-title ps-0">Pengaturan</li>
+
+        <li class="sidebar-item">
+          <a href="{{ url('/dashboard') }}" class='sidebar-link'>
+            <i class="bi bi-gear-fill"></i>
+            <span>Setting</span>
+          </a>
+        </li>
+
+        <li class="sidebar-item">
+          <a href="{{ url('/dashboard') }}" class='sidebar-link'>
+            <i class="bi bi-person-fill"></i>
+            <span>Pengguna</span>
+          </a>
         </li>
 
       </ul>
